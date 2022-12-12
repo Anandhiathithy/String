295dc56b50a0188c9fe9725585703cb93cad32fa
Anandhiathithy <109408742+Anandhiathithy@users.noreply.github.com>
2022-12-12 20:46:54 +0530

    Add files via upload

---
+++ Stringtodouble.java
@@ -0,0 +1,16 @@
+public class Stringtodouble
+{
+public static void main(String args[])
+{
+String s="100.10";
+double d=Double.parseDouble(s);
+System.out.println(s+10);
+System.out.println(d+10);
+}
+}
+
+
+output
+100.1010
+110.1
+
